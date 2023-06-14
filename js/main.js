e6c5ca32de8199c7aefcd0d5b5ea8ee92a4c5dae
unknown <surajjamdade83086@gmail.com>
2023-06-14 15:16:23 +0530

    update

--- js/main.js
+++ js/main.js
@@ -149,11 +149,56 @@
         items: 1,
       },
       768: {
-        items: 2,
+        items: 3,
       },
       992: {
-        items: 2,
+        items: 3,
       },
     },
+  });
+
+  /*=====================
+     20.Add to wishlist
+     ==========================*/
+  $(".upsouth_btn").on("click", function () {
+    $.notify(
+      {
+        icon: "fa fa-check",
+        title: "Success!",
+        message: " Successfully Login",
+      },
+      {
+        element: "body",
+        position: null,
+        type: "info",
+        allow_dismiss: true,
+        newest_on_top: false,
+        showProgressbar: true,
+        placement: {
+          from: "top",
+          align: "right",
+        },
+        offset: 20,
+        spacing: 10,
+        z_index: 1000000000,
+        delay: 5000,
+        animate: {
+          enter: "animated fadeInDown",
+          exit: "animated fadeOutUp",
+        },
+        icon_type: "class",
+        template:
+          '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+          '<button type="button" aria-hidden="true" class="btn-close" data-notify="dismiss"></button>' +
+          '<span data-notify="icon"></span> ' +
+          '<span data-notify="title">{1}</span> ' +
+          '<span data-notify="message">{2}</span>' +
+          '<div class="progress" data-notify="progressbar">' +
+          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+          "</div>" +
+          '<a href="{3}" target="{4}" data-notify="url"></a>' +
+          "</div>",
+      }
+    );
   });
 })(jQuery);
